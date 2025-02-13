67d5041010caf69df371724c89df75e97d7b69b7
yusuf <yusufcimen90@windowslive.com>
2025-02-13 18:29:59 +0300

    index carousel için view component çalışması

---
+++ App.e-commerce/ViewComponents/ProductCarouselViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.e_commerce.ViewComponents
+{
+    public class ProductCarouselViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // Fetch or generate the data needed for the view component
+            var products = await GetProductsAsync();
+            return View(products);
+        }
+
+        private Task<List<Product>> GetProductsAsync()
+        {
+            // Replace with your actual data fetching logic
+            return Task.FromResult(new List<Product>
+           {
+               new Product{Id=1,Img="img/categories/cat-1.jpg",Name="Drink Fruits"},
+                    new Product{Id=2,Img="img/categories/cat-2.jpg",Name="Dried Fruits"},
+                    new Product{Id=3,Img="img/categories/cat-3.jpg",Name="Fresh Fruits"},
+                    new Product{Id=4,Img="img/categories/cat-4.jpg",Name="Vegetables"},
+                    new Product{Id=5,Img="img/categories/cat-5.jpg",Name="Bla bla Fruits"},
+           });
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Img { get; set; }
+        public string Name { get; set; }
+    }
+}
