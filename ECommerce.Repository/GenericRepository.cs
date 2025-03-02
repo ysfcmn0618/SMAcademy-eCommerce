ea70abe2a489355e8192233ed472e40db1077476
yusuf <yusufcimen90@windowslive.com>
2025-03-02 22:01:21 +0300

    Crud işlemleri için Repository servis library yazılması

---
+++ ECommerce.Repository/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using App.Data.ECommerceDbContext;
+using ECommerce.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Data.Repository
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ECommerceDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(ECommerceDbContext context)
+        {
+            _dbContext = context;
+            _dbSet = context.Set<T>();
+        }
+        public async Task<T> Add(T entity)
+        {
+            _dbSet.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Delete(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity is not null)
+            {
+                _dbSet.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+                return entity;
+            }
+            return entity;
+            
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+           return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetById(int id)
+        {           
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ ECommerce.Repository/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
+    }
+}
